f39c0a4bf515b9e1731427b2f030b58b575233f3
chewmountain <mironov.develop@gmail.com>
2023-11-17 11:24:35 +0300

    add async and sync redux state

--- src/entities/Counter/model/selectors/getCounterValue.ts
+++ src/entities/Counter/model/selectors/getCounterValue.ts
@@ -1,4 +1,6 @@
 import { StateSchema } from "@/app/providers/StoreProvider";
 
-export const getCounterValue = (state: StateSchema) =>
+export const getCounterAsyncValue = (state: StateSchema) =>
     state.counterAsync?.value;
+
+export const getCounterValue = (state: StateSchema) => state.counterPlain.value;
--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -7,7 +7,10 @@ import { DynamicModuleLoader } from "@/shared/lib/components/DynamicModuleLoader
 import { ReducersList } from "@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
 import { HStack, VStack } from "@/shared/ui/Stack";
 
-import { getCounterValue } from "../model/selectors/getCounterValue";
+import {
+    getCounterAsyncValue,
+    getCounterValue,
+} from "../model/selectors/getCounterValue";
 import { counterActions, counterReducer } from "../model/slice/counterSlice";
 
 import styles from "./Counter.module.scss";
@@ -20,9 +23,9 @@ const reducers: ReducersList = {
     counterAsync: counterReducer,
 };
 
-export const Counter = memo(({ className }: CounterProps) => {
+const CounterAsync = memo(({ className }: CounterProps) => {
     const dispatch = useAppDispatch();
-    const value = useSelector(getCounterValue);
+    const value = useSelector(getCounterAsyncValue);
 
     const onMathHandler = (value: number) => {
         dispatch(counterActions.doMath(Number(value)));
@@ -52,3 +55,59 @@ export const Counter = memo(({ className }: CounterProps) => {
         </DynamicModuleLoader>
     );
 });
+
+const CounterPlain = memo(({ className }: CounterProps) => {
+    const dispatch = useAppDispatch();
+    const value = useSelector(getCounterValue);
+
+    const onMathHandler = (value: number) => {
+        dispatch(counterActions.doMath(Number(value)));
+    };
+
+    return (
+        <div className={classNames(styles.Counter, {}, [className])}>
+            <VStack gap="16" align="center" fullWidth>
+                <p className="text-2xl">{value}</p>
+                <HStack gap="8">
+                    <button
+                        className="p-2 bg-green-300 rounded-md"
+                        onClick={() => onMathHandler(1)}
+                    >
+                        INC
+                    </button>
+                    <button
+                        className="p-2 bg-red-300 rounded-md"
+                        onClick={() => onMathHandler(-1)}
+                    >
+                        DEC
+                    </button>
+                </HStack>
+            </VStack>
+        </div>
+    );
+});
+
+export const Counter = () => {
+    return (
+        <>
+            <div className="mb-8">
+                <h4 className="text-center">
+                    This is async reducer, than will remove after unmounting
+                    component.
+                </h4>
+                <CounterAsync />
+            </div>
+            <div className="mb-8">
+                <h4 className="text-center">This is normal reducer.</h4>
+                <CounterPlain />
+            </div>
+            <div className="mb-8">
+                <h4 className="text-center">
+                    Both using the same slice. Make some math, go to another
+                    page and then return to main. You can inspect state with
+                    Redux extensions for browser.
+                </h4>
+            </div>
+        </>
+    );
+};
