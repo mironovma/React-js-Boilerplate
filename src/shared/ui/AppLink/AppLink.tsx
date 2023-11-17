87bdb75e51d950c191c2aedd821bedcec17dcdc5
chewmountain <mironov.develop@gmail.com>
2023-11-17 11:11:25 +0300

    add stylelint

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,7 +1,6 @@
 import { classNames } from "@/shared/lib/classNames/classNames";
 import { FC, ReactNode, memo } from "react";
 import { LinkProps, NavLink } from "react-router-dom";
-import styles from "./AppLink.module.scss";
 
 export type AppLinkVarian = "primary" | "secondary" | "red";
 
@@ -24,9 +23,7 @@ export const AppLink: FC<AppLinkProps> = memo((props) => {
         <NavLink
             to={to}
             className={({ isActive }) =>
-                classNames(styles.AppLink, { [activeClassName]: isActive }, [
-                    className,
-                ])
+                classNames("", { [activeClassName]: isActive }, [className])
             }
             {...otherProps}
         >
